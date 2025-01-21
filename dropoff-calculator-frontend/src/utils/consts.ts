76f845d8073b0dd15e95c8e07108af32ec90f3db
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2025-01-22 05:16:24 +0800

    Update consts.ts
    Added a way to check ENV

--- dropoff-calculator-frontend/src/utils/consts.ts
+++ dropoff-calculator-frontend/src/utils/consts.ts
@@ -1,4 +1,6 @@
-export const apiURL: string = "https://api.dropoff.delivery";
+export const apiURL: string = import.meta.env.DEV
+  ? "http://localhost:5050"
+  : "https://api.dropoff.delivery";
 
 // "https://api.dropoff.delivery";
 // For dev env set this to "http://localhost:5050"
